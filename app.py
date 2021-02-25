f285c1cafdbc1f7f720ee55a12942842465af00c
Vikram <veryseriousemailaddress@gmail.com>
2021-02-25 15:38:00 +0000

    It works innit

--- app.py
+++ app.py
@@ -12,16 +12,14 @@ app =  Flask(__name__)
 
 authJWT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJuYmYiOjE2MTM5ODQ0MDAsImFwaV9zdWIiOiIxYTAwMWJkNTEzODE3MDdjMzBhZmQ2M2NhYmYwNzc3NGU1MGY1YWU4YzM0NzhlYTQzYmJiY2MzNTdkMThlMDI2MTYxNDM3MzIwMDAwMCIsInBsYyI6IjVkY2VjNzRhZTk3NzAxMGUwM2FkNjQ5NSIsImV4cCI6MTYxNDM3MzIwMCwiZGV2ZWxvcGVyX2lkIjoiMWEwMDFiZDUxMzgxNzA3YzMwYWZkNjNjYWJmMDc3NzRlNTBmNWFlOGMzNDc4ZWE0M2JiYmNjMzU3ZDE4ZTAyNiJ9.hX06Qo6Yy5ROOysp70VxQ02f1d3eZDGAFEIBFG-0tI5iPXghinLMgGJM3FnEPUacaxutWmb6w4lXm1gNnR5ejPf_6e92Dp4IpB3_bb-h3X-OvkHPM6vTcTQh3uVsdyk51iT9FFnsj7R2vk-0BnE-BBQrbywphWY2AXdik0UqkK-Rza9QhtDrZV8GOftncDxjaDyElyCWivMnSMgq7y2QaKB_8-Lu7vlhMYxXET8bRX6PUKKdRLXncdNgQ0j8fYQPqI12sMaCrMJOuv0eRCBzBqqzQEdODBVTMCGMkI9B2bAYWvOqu_xS2EzEYwLL_mpkrigqsRoBHvK8vBMAoU9Gog'
 conn = http.client.HTTPSConnection("sandbox.capitalone.co.uk")
-accountID = 31540673
-
 
 @app.route('/')
 def start():
     return render_template('index.html')
 
-@app.route('/dashboard')
-def dashboard():
-
+@app.route('/dashboard/<name>')
+def dashboard(name):
+    accountID = str(name)
     response = None
     payload = ''
     headers = {
@@ -41,27 +39,33 @@ def dashboard():
     #STAGE 1 OF BEING ELIGIBLE FOR REWARDS
     #566 is what TransUnion classifies as a fair
     if (int(creditScore) < 566):
-        response = '{"response": "Failure", "stage": "1"}'
+        response = 'False'
         return render_template('dashboard.html', data=response)
     else: 
-        conn.request("GET", "/developer-services-platform-pr/api/data/transactions/accounts/{}/transactions".format(accountID), payload, headers)
-        res = conn.getresponse()
-        data = res.read()
+        response = 'True'
+        return render_template('dashboard.html', data=response)
 
+        '''
+        # STAGE 2 OF BEING ELIGIBLE FOR REWARDS
+        # We take your last 6 months of transactions and 
         eligbleTransactions = []
         tmp = (json.loads(data)['Transactions'])
 
-        now = datetime.date.today()
+        now = datetime.datetime.now()
 
-        print(now)
-        '''
-        now = datetime.date.today()
-        sixmonthsago = add_months(now, -6)
-
-        print (sixmonthsago)
+        sixmonthsago = (now - datetime.timedelta(days=182))
+        
+        print(sixmonthsago)
+        format = "%Y-%m-%d %H:%M:%S"
         for transaction in tmp:
-            print(transaction['timestamp'])
+            tmptime = datetime.datetime.strptime(transaction['timestamp'], format)
+            
+            print(transaction)
+            if (tmptime > sixmonthsago):
+                print(transaction)
         '''
+
+            
 
     return render_template('dashboard.html', data=response)
 
--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,8 @@
 function Say_Hello() {
+
+    var x =   document.getElementById("fname").value; 
+
     const url = window.location.href;
     console.log(url)
-    window.location.replace(url + "dashboard");
+    window.location.replace(url + "dashboard/" + x);
 }
