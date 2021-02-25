e1a2c4e2de3f6c34c690e9e9076887dfeb1550f5
Vikram <veryseriousemailaddress@gmail.com>
2021-02-25 12:33:48 +0000

    Clean code

--- app.py
+++ app.py
@@ -3,11 +3,14 @@ load_dotenv()
 from os import environ
 from json import loads as toDict
 from flask import Flask, render_template, request, jsonify
-
 import http.client
 import mimetypes
 
 app =  Flask(__name__)
+
+authJWT = ''
+conn = http.client.HTTPSConnection("sandbox.capitalone.co.uk")
+accountID = 31540673
 
 @app.route('/')
 def start():
@@ -16,9 +19,6 @@ def start():
 @app.route('/dashboard')
 def dashboard():
 
-    authJWT = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJuYmYiOjE2MTM5ODQ0MDAsImFwaV9zdWIiOiIxYTAwMWJkNTEzODE3MDdjMzBhZmQ2M2NhYmYwNzc3NGU1MGY1YWU4YzM0NzhlYTQzYmJiY2MzNTdkMThlMDI2MTYxNDM3MzIwMDAwMCIsInBsYyI6IjVkY2VjNzRhZTk3NzAxMGUwM2FkNjQ5NSIsImV4cCI6MTYxNDM3MzIwMCwiZGV2ZWxvcGVyX2lkIjoiMWEwMDFiZDUxMzgxNzA3YzMwYWZkNjNjYWJmMDc3NzRlNTBmNWFlOGMzNDc4ZWE0M2JiYmNjMzU3ZDE4ZTAyNiJ9.hX06Qo6Yy5ROOysp70VxQ02f1d3eZDGAFEIBFG-0tI5iPXghinLMgGJM3FnEPUacaxutWmb6w4lXm1gNnR5ejPf_6e92Dp4IpB3_bb-h3X-OvkHPM6vTcTQh3uVsdyk51iT9FFnsj7R2vk-0BnE-BBQrbywphWY2AXdik0UqkK-Rza9QhtDrZV8GOftncDxjaDyElyCWivMnSMgq7y2QaKB_8-Lu7vlhMYxXET8bRX6PUKKdRLXncdNgQ0j8fYQPqI12sMaCrMJOuv0eRCBzBqqzQEdODBVTMCGMkI9B2bAYWvOqu_xS2EzEYwLL_mpkrigqsRoBHvK8vBMAoU9Gog'
-    accountID = 31540673
-    conn = http.client.HTTPSConnection("sandbox.capitalone.co.uk")
     payload = ''
     headers = {
     'Authorization': 'Bearer {}'.format(authJWT),
